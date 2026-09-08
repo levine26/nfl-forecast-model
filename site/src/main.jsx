a1000f4a8f816ff5ea812e15338d859210785f99
levine26 <levine26@icloud.com>
2026-09-08 14:15:31 -0700

    Switch dashboard to evidence-preview v3

--- site/src/main.jsx
+++ site/src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './AppV2.jsx'
+import App from './AppV3.jsx'
 import './styles.css'
+import './v3.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
