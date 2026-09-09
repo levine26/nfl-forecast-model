509686e81388c480e1022d276c5c9e49433c0fe7
levine26 <levine26@icloud.com>
2026-09-09 14:49:09 -0700

    Clean up History calibration presentation

--- site/src/main.jsx
+++ site/src/main.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './AppPublicationVNext.jsx'
+import './calibration-polish.css'
+import { installCalibrationPresentationAdapter } from './calibration-presentation.js'
+
+installCalibrationPresentationAdapter()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
