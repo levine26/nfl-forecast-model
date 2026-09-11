0889bf5be9ae97152fe955a6d9b35b62e2242ab4
levine26 <levine26@icloud.com>
2026-09-11 16:49:58 -0700

    Keep LevLine 3.0 launch UI-neutral

--- site/src/main.jsx
+++ site/src/main.jsx
@@ -1,10 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './AppCoherent.jsx'
-import './levline-3-launch.css'
-
-document.title = 'Sunday Signal — Powered by LevLine 3.0'
-document.documentElement.dataset.levlineVersion = '3.0'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
