38a3ecfda7e4ed869dddcb77cd29d57f0b7d82a7
levine26 <levine26@icloud.com>
2026-09-10 18:01:57 -0700

    Activate coherent Sunday Signal frontend

--- site/src/main.jsx
+++ site/src/main.jsx
@@ -1,12 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './AppPublicationVNext.jsx'
-import './calibration-polish.css'
-import { installCalibrationPresentationAdapter } from './calibration-presentation.js'
-import { installFstProductionPresentationAdapter } from './fst-production-presentation.js'
-
-installCalibrationPresentationAdapter()
-installFstProductionPresentationAdapter()
+import App from './AppCoherent.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
