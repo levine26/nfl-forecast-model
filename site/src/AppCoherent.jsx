e50632ffc32b3ce1753cc8f9892ee370e08a40fd
levine26 <levine26@icloud.com>
2026-09-12 16:29:46 -0700

    Register DOM compatibility bridge before experience layer

--- site/src/AppCoherent.jsx
+++ site/src/AppCoherent.jsx
@@ -52,7 +52,7 @@ export default function AppCoherent() {
     <AppSignal/>
     <SignalEnhancements/>
     <ForecastClarity/>
-    <ExperienceLayer/>
     <ExperienceDomFixes/>
+    <ExperienceLayer/>
   </>
 }
