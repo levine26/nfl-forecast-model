405d77e7db7813c63035e8aed75c3e166ba5f655
levine26 <levine26@icloud.com>
2026-09-11 22:26:03 -0700

    Keep AppCoherent as canonical production entrypoint

--- site/src/main.jsx
+++ site/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './AppSignal.jsx'
+import App from './AppCoherent.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
