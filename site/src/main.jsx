7edc8aaf626575a4c1b47b4d23c8f5c158399cf0
levine26 <levine26@icloud.com>
2026-09-11 22:21:10 -0700

    Switch production entry point to redesigned Sunday Signal UI

--- site/src/main.jsx
+++ site/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './AppCoherent.jsx'
+import App from './AppSignal.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
